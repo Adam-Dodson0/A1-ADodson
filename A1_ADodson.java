3255334bc34b9148bf713dace9e49c94bd1debe8
Adam.D-091 <144310494+Adam-Dodson0@users.noreply.github.com>
2026-07-10 15:51:18 +1000

    Second Commit message

---
+++ A1_ADodson.java
@@ -0,0 +1,17 @@
+/*
+    This is a template for the A1_ADodson.java file.
+    That displays my knowledge with Java programming.
+    Created by: Adam Dodson
+    Date: 1/17/2024
+    Project: Is about creating an Appointment System for a Health Service.
+    The system will allow users to create, view, and manage appointments with healthcare providers.
+    It will include features such as appointment scheduling, reminders, and patient information management.
+ */
+public class A1_ADodson{
+    
+    public static void main(String[] args){
+        
+        
+    }
+
+}

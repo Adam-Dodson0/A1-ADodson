b0fcfe5b2e676e55d602ddd0f32adc02b8057010
Adam.D-091 <144310494+Adam-Dodson0@users.noreply.github.com>
2026-07-10 18:29:51 +1000

    Add initial implementation of A1_ADodson.java for appointment system

---
+++ A1-ADodson.java
@@ -0,0 +1,15 @@
+/*
+    Name: Adam Dodson
+    date: 10/07/2026
+    Assignment 1 for PROG2004-T3
+    Description: This is a Java program that is for a Appointment System for a Health Service.
+    This program will allow the user to view, and manage appointments for patients and doctors.
+    Viewing existing appointments, and canceling appointments.
+
+*/
+
+class A1_ADodson {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

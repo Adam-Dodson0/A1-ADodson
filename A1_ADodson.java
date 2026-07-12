48629a80a7b20c3972209e593ef317df9e6130ab
Adam.D-091 <144310494+Adam-Dodson0@users.noreply.github.com>
2026-07-12 10:36:57 +1000

    removed enum since we arent being taught to enumeration and changed it to List Arrays. Added all medical professional roles with getters/setters

--- A1_ADodson.java
+++ A1_ADodson.java
@@ -13,19 +13,15 @@
     import java.util.Arrays;
     import java.util.List;
 
-    enum weekDay { 
-        Monday, Tuesday, Wednesday, Thursday, Friday;
-    }
-
 class A1_ADodson {
     public static void main(String[] args) {
 
         //Days available to work
-        List<weekDay> mtw = Arrays.asList(weekDay.Monday, weekDay.Tuesday, weekDay.Wednesday);
-        List<weekDay> mwf = Arrays.asList(weekDay.Monday, weekDay.Wednesday, weekDay.Friday);
-        List<weekDay> tt = Arrays.asList(weekDay.Tuesday, weekDay.Thursday);
-        List<weekDay> WeekDays = Arrays.asList(weekDay.Monday, weekDay.Tuesday, weekDay.Wednesday, weekDay.Thursday,
-                                            weekDay.Friday);
+        List<String> mtw = Arrays.asList("Monday", "Tuesday", "Wednesday");
+        List<String> mwf = Arrays.asList("Monday", "Wednesday", "Friday");
+        List<String> tt = Arrays.asList("Tuesday", "Thursday");
+        List<String> weekDays = Arrays.asList("Monday", "Tuesday", "Wednesday",
+                                           "Thursday", "Friday");
 
         List<String> availableSlots = new ArrayList<>(Arrays.asList(
         "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30",
@@ -33,38 +29,77 @@ class A1_ADodson {
         "03:00", "03:30", "04:00"
         ));
  
-        //Nurse/Receptionist
-        Nurse nurse1 = new Nurse("1010", "Betty Sanchez", WeekDays);
-        Nurse nurse2 = new Nurse("1025", "Harry Rumble", tt);
-        Nurse nurse3 = new Nurse("1097","Melanie Cherkov", mwf);
+        //Medical Professionals grouped by ID, Name, Days they work, AssignedRoom
+        Nurse nurse1 = new Nurse("9010", "Norelle Sanchez", weekDays, new ArrayList<>(availableSlots),"");
+        Nurse nurse2 = new Nurse("9025", "Harry Rumble", tt, new ArrayList<>(availableSlots),"Operating Ward");
+        Nurse nurse3 = new Nurse("9097","Melanie Cherkov", mwf, new ArrayList<>(availableSlots),"Emergency Ward");
+        Nurse nurse4 = new Nurse("9845", "Betty Suarez", mtw,new ArrayList<>(availableSlots),"Children's Ward");
 
-        //Doctors grouped by ID, Name, Days they work, AssignedRoom
-        Practitoner gp1 = new Practitoner("1000", "Dr. Anthony Reed", WeekDays, "Room 3", new ArrayList<>(availableSlots));
-        Practitoner gp2 = new Practitoner("2003", "Dr. Greg House", tt, "Room 1", new ArrayList<>(availableSlots));
-        Practitoner gp3 = new Practitoner("3006", "Dr. JD", mtw, "Room 5", new ArrayList<>(availableSlots));
-        Practitoner gp4 = new Practitoner("4245", "Dr. Candice Robin", mwf, "Room 4", new ArrayList<>(availableSlots));
+        Practitoner gp1 = new Practitoner("1126", "Dr. Anthony Reed", weekDays, new ArrayList<>(availableSlots), "Room 3");
+        Practitoner gp2 = new Practitoner("1175", "Dr. Greg House", tt, new ArrayList<>(availableSlots), "Room 1");
+        Practitoner gp3 = new Practitoner("1257", "Dr. JD", mtw, new ArrayList<>(availableSlots), "Room 5");
+        Practitoner gp4 = new Practitoner("1245", "Dr. Candice Robin", mwf, new ArrayList<>(availableSlots), "Room 4");
 
+        Specialist sp1 = new Specialist("2423","Dr. Shannon Templeton ",tt, new ArrayList<> (availableSlots),"Room 2");
+        Specialist sp2 = new Specialist("2256","Dr. Ethan Holmes ",mwf, new ArrayList<> (availableSlots),"Room 10");
+        Specialist sp3 = new Specialist("2912","Dr. Tuesday Addams",mtw, new ArrayList<> (availableSlots),"Room 9");
+
+        Surgeon sg1 = new Surgeon("3725", "Dr. Jordan", tt, new ArrayList<>(availableSlots),"Operating Room 1");
+        Surgeon sg2 = new Surgeon("3982", "Dr. Ophelia", mwf, new ArrayList<>(availableSlots),"Operating Room 2");
+        
         //Patients
         Patient p1 = new Patient ("Tyler Craddle", "0410123111");
         Patient p2 = new Patient ("John Nolan", "0412342199");
         Patient p3 = new Patient ("Jimmy Barnes", "0414852745");
         Patient p4 = new Patient ("Alicia Dixon", "0451846852");
 
+        List<HealthProfessional> professionals = new ArrayList<>();
+        professionals.add(nurse1);
+        professionals.add(nurse2);
+        professionals.add(nurse3);
+        professionals.add(nurse4);
+        professionals.add(gp1);
+        professionals.add(gp2);
+        professionals.add(gp3);
+        professionals.add(gp4);
+        professionals.add(sp1);
+        professionals.add(sp2);
+        professionals.add(sp3);
+        professionals.add(sg1);
+        professionals.add(sg2);
+
+        for (HealthProfessional hp : professionals) {
+            System.out.println(hp);
+            System.out.println(" ->" + hp.performSpecialFunction());
+        }
+
     }
 }
 
-class Practitoner {
-
-    private String id;
-    private String name;
-    public List<weekDay> WeekDays;
-    private final String consultingRoom;
+abstract class HealthProfessional {
+    
+    final String id;
+    final String name;
+    private final List<String> weekDays;
     private final List<String> availableSlots;
 
-    public Practitoner(String id, String name, List<weekDay> WeekDays, String consultingRoom, List<String> availableSlots) {
-            this.consultingRoom = (consultingRoom == null || consultingRoom.trim().isEmpty())
-            ? " " : consultingRoom;
-            this.availableSlots = availableSlots;
+    public HealthProfessional(String id, String name, List<String> weekDays, List<String> availableSlots) {
+        if (id == null || id.isEmpty() || !id.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("ID can only be numbers.");
+        }
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        if (weekDays == null || weekDays.isEmpty()) {
+            throw new IllegalArgumentException("Health Professionals must be on Roster.");
+        }
+        if (availableSlots == null || availableSlots.isEmpty()) {
+            throw new IllegalArgumentException("Health Professionals must have Appointments.");
+        }
+        this.id = id;
+        this.name = name;
+        this.weekDays = weekDays;
+        this.availableSlots = availableSlots;
     }
 
     public String getId() {
@@ -75,21 +110,107 @@ class Practitoner {
         return name;
     }
 
-    public List<weekDay> getWeekDays() {
-        return new ArrayList<>(WeekDays);
+    public List<String> getWeekDays() {
+        return weekDays;
+    }
+
+    public List<String> getAvailableSlots() {
+        return availableSlots;
+    }
+
+    abstract String performSpecialFunction();
+}
+
+class Nurse extends HealthProfessional{
+
+    private final String ward;
+
+    public Nurse (String id, String name, List<String> weekDays,
+                  List<String> availableSlots, String ward) {
+                    super(id, name, weekDays, availableSlots);
+        this.ward = (ward == null || ward.trim().isEmpty()) ? "General Ward" : ward;
+    }
+
+    public String getWard() {
+        return ward;
+    }
+
+    @Override
+    public String performSpecialFunction() {
+        return getName() + "Takes a blood sample, monitors vitals and gives fluid in " + ward + ".";
+    }
+    }
+
+class Practitoner extends HealthProfessional {
+
+    private final String consultingRoom;
+
+    public Practitoner (String id, String name, List<String> weekDays,
+                        List<String> availableSlots, String consultingRoom) {
+            super(id, name, weekDays, availableSlots);
+            this.consultingRoom = (consultingRoom == null || consultingRoom.trim().isEmpty())
+            ? " " : consultingRoom;
     }
 
     public String getConsultingRoom() {
         return consultingRoom;
     }
 
-    public List<String> getAvailableSlots(){
-        return availableSlots;
+    @Override
+    public String performSpecialFunction() {
+        return getName() + "Performs Health Check on patient and prescribes necessary medications.";
     }
 
     @Override
     public String toString() {
         return name + "(ID: " + id + "|" + consultingRoom + ")";
+    }
+}
+
+class Specialist extends HealthProfessional {
+    
+    private final String consultingRoom;
+
+    public Specialist(String id, String name, List<String> weekDays,
+                      List<String> availableSlots, String consultingRoom) {
+        super(id, name, weekDays, availableSlots);    
+        this.consultingRoom = (consultingRoom == null || consultingRoom.trim().isEmpty())
+            ? " " : consultingRoom;
+    }
+
+    public String getConsultingRoom() {
+        return consultingRoom;
+    }
+
+    @Override
+    public String performSpecialFunction() {
+        return getName() + "Looks over Patient's records and advises suitable treatment plans.";
+    }
+
+    @Override
+    public String toString() {
+        return name + "(ID: " + id + "|" + consultingRoom + ")";
+    }
+}
+
+class Surgeon extends HealthProfessional{
+    
+    private final String consultingRoom; //the consulting Room is the Operating Room
+
+    public Surgeon (String id, String name, List<String> weekDays,
+                   List<String> availableSlots, String consultingRoom) {
+        super(id, name, weekDays, availableSlots);
+        this.consultingRoom = (consultingRoom == null || consultingRoom.trim().isEmpty())
+            ? "Operating Room" : consultingRoom;    
+    }
+
+    public String getConsultingRoom() {
+        return consultingRoom;
+    }
+
+    @Override
+    public String performSpecialFunction() {
+        return getName() + "Performing operation in " + getConsultingRoom();
     }
 }
 
@@ -127,21 +248,21 @@ class Patient {
 abstract class Appointment implements Comparable<Appointment> {
 
     final Patient patient;
-    final Practitoner practitoner;
-    final availableSlots slot;
+    final HealthProfessional professionals;
+    final String slot;
 
-    public Appointment(Patient patient, Practitoner practitoner, availableSlots slot) {
+    public Appointment(Patient patient, HealthProfessional professionals, String slot) {
         if (patient == null) {
             throw new IllegalArgumentException("Appointment must have a patient");
         }
-        if (practitoner == null) {
+        if (professionals == null) {
             throw new IllegalArgumentException("Appointment need a Doctor");
         }
         if (slot == null) {
             throw new IllegalArgumentException("Appointments must be alloted a time slot");
         }
         this.patient = patient;
-        this.practitoner = practitoner;
+        this.professionals = professionals;
         this.slot = slot;
     }
 
@@ -149,19 +270,22 @@ abstract class Appointment implements Comparable<Appointment> {
         return patient;
     }
 
-    public Practitoner getPractitoner() {
-        return practitoner;
+    public HealthProfessional getProfessional() {
+        return professionals;
     }
 
-    public availableSlots getSlot() {
+    public String getSlot() {
         return slot;
+    }
+
+    @Override
+    public int compareTo(Appointment taken) {
+        return this.slot.compareTo(taken.slot);
     }
 
     @Override
     public String toString() {
         return "[" + slot + "] " + patient + "\n"
-        + " With ->" + practitoner + "\n";
-    }
-}
+        + " With ->" + professionals + "\n";
     }
 }
